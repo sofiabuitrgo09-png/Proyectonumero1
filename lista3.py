03381c50c5e0905e87f34fdd1352824edb6ca083
sofiabuitrgo09-png <sofiabuitrgo09@gmail.com>
2025-11-24 17:37:17 -0500

    Add files via upload

---
+++ lista3.py
@@ -0,0 +1,10 @@
+frase = 'esto es un string "cadena de caracteres'
+
+print (len(frase))
+print (frase[0])
+print ("X" in frase)
+
+print (type(frase))
+print (type(frase [0]))
+
+print(frase.upper())
